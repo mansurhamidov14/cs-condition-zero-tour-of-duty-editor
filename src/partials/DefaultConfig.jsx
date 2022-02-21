3d0df24e5ee9c9c1eee5e42f93f08b71ea0cedce
mansurhamidov14 <mansur.hamidoff.1994@gmail.com>
2022-02-22 01:14:41 +0200

    refactoring

--- src/partials/DefaultConfig.jsx
+++ src/partials/DefaultConfig.jsx
@@ -1,32 +1,62 @@
 import React from 'react';
-import { HTMLSelect, InputGroup, Label, NumericInput, Slider } from "@blueprintjs/core";
-import { FIELDS } from "../consts";
+import { Button, ControlGroup, H5, HTMLSelect, InputGroup, Label, NumericInput, Slider } from "@blueprintjs/core";
+import { FIELDS, WEAPONS } from "../consts";
 import { Col, Row } from "../components";
+
+const weaponPreference = ['none'];
 
 export const DefaultConfig = () => {
 	const [skill, setSkill] = React.useState(40);
 	return (
 		<Row className="py-1">
-			{FIELDS.map((field) => (
-				<Col size={4} key={field.accessor}>
-					<Label>
-						{field.label}
-						{field.type === 'select' ? (
-							<HTMLSelect large>
-								{field.options.map((opt) => (
-									<option value={opt.value}>{opt.label}</option>
-								))}
-							</HTMLSelect>
-						) : field.type === 'slider' ? (
-							<Slider {...field.props} value={skill} onChange={setSkill} />
-						) : field.type === 'number' ? (
-							<NumericInput name={field.accessor} large fill {...field.props} />
-						) : (
-							<InputGroup type={field.type} name={field.accessor} large />
-						)}
-					</Label>
-				</Col>
-			))}
+      <Col size={12}>
+        <H5>Configs</H5>
+        <Row>
+          {FIELDS.map((field) => (
+            <Col size={4} key={field.accessor}>
+              <Label>
+                {field.label}
+                {field.type === 'select' ? (
+                  <HTMLSelect large>
+                    {field.options.map((opt) => (
+                      <option value={opt.value}>{opt.label}</option>
+                    ))}
+                  </HTMLSelect>
+                ) : field.type === 'slider' ? (
+                  <Slider {...field.props} value={skill} onChange={setSkill} />
+                ) : field.type === 'number' ? (
+                  <NumericInput name={field.accessor} large fill {...field.props} />
+                ) : (
+                  <InputGroup type={field.type} name={field.accessor} large />
+                )}
+              </Label>
+            </Col>
+          ))}
+        </Row>
+      </Col>
+      <Col size={12} className="py-2">
+        <H5>Weapon preference</H5>
+        <Row>
+          {weaponPreference.map((value, index) => {
+            return (
+              <Col size={4}>
+                <ControlGroup fill>
+                  <HTMLSelect value={value} large fill>
+                    <option value="none">None</option>
+                    {WEAPONS.map((weapon) => (
+                      <option key={weapon.value} value={weapon.value}>{weapon.label}</option>
+                    ))}
+                  </HTMLSelect>
+                  {Boolean(index) && <Button intent="danger" icon="minus" />}
+                </ControlGroup>
+              </Col>
+            )
+          })}
+          <Col size={12} className="py-1">
+            <Button intent="success" icon="plus" small>Add weapon preference</Button>
+          </Col>
+        </Row>
+      </Col>
 		</Row>
 	);
 };
--- src/partials/Players.jsx
+++ src/partials/Players.jsx
@@ -3,6 +3,7 @@ import {
   Button,
   Card,
   Checkbox,
+  Collapse,
   ControlGroup,
   H4,
   H5,
@@ -13,7 +14,7 @@ import {
   Slider
 } from "@blueprintjs/core";
 import { Col, Row } from "../components";
-import { FIELDS } from "../consts";
+import { FIELDS, WEAPONS } from "../consts";
 
 const players = new Array(3).fill(1);
 
@@ -30,71 +31,111 @@ const templates = [
   }
 ];
 
+const weaponPreference = ['none'];
+
 export const Players = () => {
 	const [skill, setSkill] = React.useState(40);
+  const [open, setOpen] = React.useState({});
   return (
     <Row>
-      {players.map(player => {
+      {players.map((player, index) => {
         return (
           <Col key={player} size={6} className="py-1">
             <Card>
-              <H4>Some player</H4>
-              <Row>
-                <Col size={6}>
-                  <Label>
-                    Player name
-                    <InputGroup type="text" />
-                  </Label>
+              <Row className="justify-content-between">
+                <Col size={9}>
+                  <H4>Some player</H4>
                 </Col>
-                <Col size={12}>
-                  <H5>Templates</H5>
-                  <Row>
-                    {['1', '2'].map(playerTemplate => {
-                      return (
-                        <Col key={playerTemplate} size={6}>
-                          <ControlGroup fill>
-                            <HTMLSelect fill>
-                              {templates.map((template) => (
-                                <option key={template.name} value={template.name}>{template.name}</option>
-                              ))}
-                            </HTMLSelect>
-                            <Button icon="minus" intent="danger" />
-                          </ControlGroup>
-                        </Col>
-                      )
-                    })}
-                    <Col size={4} className="py-1">
-                      <Button intent="success" icon="plus" small>Add template</Button>
-                    </Col>
-                  </Row>
-                </Col>
-                <Col size={12} className="py-2">
-                  <H5>Configs</H5>
-                  <Row>
-                    {FIELDS.map((field) => (
-                      <Col className="py-1" size={6} key={field.accessor}>
-                        <Label>
-                          {field.label}
-                          {field.type === 'select' ? (
-                            <HTMLSelect>
-                              {field.options.map((opt) => (
-                                <option value={opt.value}>{opt.label}</option>
-                              ))}
-                            </HTMLSelect>
-                          ) : field.type === 'slider' ? (
-                            <Slider small {...field.props} value={skill} onChange={setSkill} />
-                          ) : field.type === 'number' ? (
-                            <NumericInput name={field.accessor} fill {...field.props} />
-                          ) : (
-                            <InputGroup type={field.type} name={field.accessor} />
-                          )}
-                        </Label>
-                        <Checkbox>Inherit from template</Checkbox>
-                      </Col>
-                    ))}
-                  </Row>
+                <Col className="px-1">
+                  <Button icon="chevron-down" onClick={() => setOpen(state => ({ ...state, [index]: !state[index] }))} />
                 </Col>
               </Row>
+              <Collapse isOpen={open[index]}>
+                <Row>
+                  <Col size={6}>
+                    <Label>
+                      Player name
+                      <InputGroup type="text" />
+                    </Label>
+                  </Col>
+                  <Col size={12}>
+                    <H5>Templates</H5>
+                    <Row>
+                      {['1', '2'].map(playerTemplate => {
+                        return (
+                          <Col key={playerTemplate} size={6}>
+                            <ControlGroup fill>
+                              <HTMLSelect fill>
+                                {templates.map((template) => (
+                                  <option key={template.name} value={template.name}>{template.name}</option>
+                                ))}
+                              </HTMLSelect>
+                              <Button icon="minus" intent="danger" />
+                            </ControlGroup>
+                          </Col>
+                        )
+                      })}
+                      <Col size={4} className="py-1">
+                        <Button intent="success" icon="plus" small>Add template</Button>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col size={12} className="py-2">
+                    <H5>Configs</H5>
+                    <Row>
+                      {FIELDS.map((field) => (
+                        <Col className="py-1" size={6} key={field.accessor}>
+                          <Label>
+                            {field.label}
+                            {field.type === 'select' ? (
+                              <HTMLSelect>
+                                {field.options.map((opt) => (
+                                  <option value={opt.value}>{opt.label}</option>
+                                ))}
+                              </HTMLSelect>
+                            ) : field.type === 'slider' ? (
+                              <Slider small {...field.props} value={skill} onChange={setSkill} />
+                            ) : field.type === 'number' ? (
+                              <NumericInput name={field.accessor} fill {...field.props} />
+                            ) : (
+                              <InputGroup type={field.type} name={field.accessor} />
+                            )}
+                          </Label>
+                          <Checkbox>Inherit from template</Checkbox>
+                        </Col>
+                      ))}
+                    </Row>
+                  </Col>
+                  <Col size={12}>
+                    <H5>Weapon preference</H5>
+                    <Checkbox>Inherit weapon preference from template</Checkbox>
+                    <Row class="py-1">
+                      {true && (
+                        <>
+                          {weaponPreference.map((value) => {
+                            return (
+                              <Col size={6}>
+                                <ControlGroup fill>
+                                  <HTMLSelect value={value} fill>
+                                    <option value="none">None</option>
+                                    {WEAPONS.map((weapon) => (
+                                      <option key={weapon.value} value={weapon.value}>{weapon.label}</option>
+                                    ))}
+                                  </HTMLSelect>
+                                  <Button intent="danger" icon="minus" />
+                                </ControlGroup>
+                              </Col>
+                            )
+                          })}
+                          <Col size={12} className="py-1">
+                            <Button intent="success" icon="plus" small>Add weapon preference</Button>
+                          </Col>
+                        </>
+                      )}  
+                    </Row>
+                  </Col>
+                </Row>
+              </Collapse>
             </Card>
           </Col>
         );
